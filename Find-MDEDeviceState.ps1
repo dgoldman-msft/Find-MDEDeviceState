71ebf38982cc482e8a53c95a0230b0dece02115d
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2022-09-08 12:27:15 -0400

    Changes
    Added switch to display results to scree
    Added switch to save results
    Added object for searching for selective computers or all
    Updated examples
    Updated README.md

--- Find-MDEDeviceState.ps1
+++ Find-MDEDeviceState.ps1
@@ -6,6 +6,9 @@
 	.DESCRIPTION
 		Find the MDE onboarding state of a device
 
+	.PARAMETER ComputerName
+		Name of computer or computers you want to search
+
 	.PARAMETER DisableProgressBar
 		Disables the progress bar
 
@@ -15,15 +18,36 @@
 	.PARAMETER FailureLoggingPath
 		Failure log path
 
+	.PARAMETER SaveResults
+		Save results to file
+
+	.PARAMETER ShowResults
+		Display results to the console
+
 	.EXAMPLE
 		Find-MDEDeviceState -DisableProgressBar
 
 		This will disable the progress bar in the UI
 
 	.EXAMPLE
+		Find-MDEDeviceState -ComputerName MachineOne, MachineTwo
+
+		This will query for both MachineOne and MachineTwo
+
+	.EXAMPLE
 		Find-MDEDeviceState -Verbose
 
 		This will run the script in verbose mode
+
+	.EXAMPLE
+		Find-MDEDeviceState -ShowResults
+
+		Display search results to the console
+
+	.EXAMPLE
+		Find-MDEDeviceState -SaveResults
+
+		Query for MDE information and save results to disk
 
 	.NOTES
 		Data is saved to the $env:Temp location of the user that executed the script
@@ -34,6 +58,9 @@
 	[OutputType('System.String')]
 	[OutputType('System.IO.File')]
 	param(
+		[object[]]
+		$ComputerName,
+
 		[switch]
 		$DisableProgressBar,
 
@@ -41,20 +68,33 @@
 		$LoggingPath = "$env:Temp\MDEOnboardingState.csv",
 
 		[string]
-		$FailureLoggingPath = "$env:Temp\FailedConnections.csv"
+		$FailureLoggingPath = "$env:Temp\FailedConnections.csv",
+
+		[switch]
+		$SaveResults,
+
+		[switch]
+		$ShowResults
 	)
 
 	begin {
 		Write-Output "Starting MDE discovery process"
 		$parameters = $PSBoundParameters
+		$successfulConnectionsFound = 0
+		$failedConnectionsFound = 0
 		[System.Collections.ArrayList]$computerObjects = @()
 		[System.Collections.ArrayList]$failedConnections = @()
 	}
 
 	process {
 		try {
-			Write-Verbose "Retrieving computer list"
-			$computers = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+			if (-NOT ($ComputerName)) {
+				Write-Verbose "No computer name passed in. Retrieving full domain computer list"
+				$computers = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name -ErrorAction Stop
+			}
+			else {
+				$computers = $ComputerName
+			}
 
 			foreach ($computer in $computers) {
 				if (-NOT ($parameters.ContainsKey('DisableProgressBar'))) {
@@ -77,7 +117,7 @@
 				}
 				else {
 					$null = $connection.OnboardedInfo -match '(orgId\\).*(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})'
-					if($matches[0]){ $($orgId = $matches[0]).ToString() } else { $($orgId = "Not Found" ).ToString()}
+					if ($matches[0]) { $orgId = $matches[0].ToString() } else { $orgId = "Not Found".ToString() }
 
 					$machineInfo = [PSCustomObject]@{
 						MachineName   = $computer
@@ -89,14 +129,20 @@
 					$null = $computerObjects.Add($machineInfo)
 				}
 			}
+
+			if ($parameters.ContainsKey('ShowResults')) {
+				$computerObjects | Select-Object MachineName, SenseGuid, SenseId, OnboardedInfo | Sort-Object -Property MachineName | Format-Table
+			}
 		}
 		catch {
 			Write-Output "Error: $_"
 		}
 
 		try {
-			[PSCustomObject]$computerObjects | Export-Csv -Path $LoggingPath -ErrorAction Stop -Encoding UTF8 -NoTypeInformation -Append
-			[PSCustomObject]$failedConnections | Export-Csv -Path $FailureLoggingPath -ErrorAction Stop -Encoding UTF8 -NoTypeInformation -Append
+			if ($parameters.ContainsKey('SaveResults')) {
+				[PSCustomObject]$computerObjects | Export-Csv -Path $LoggingPath -ErrorAction Stop -Encoding UTF8 -NoTypeInformation -Append
+				[PSCustomObject]$failedConnections | Export-Csv -Path $FailureLoggingPath -ErrorAction Stop -Encoding UTF8 -NoTypeInformation -Append
+			}
 		}
 		catch {
 			Write-Output "Error: $_"
@@ -104,10 +150,12 @@
 	}
 
 	end {
-		Write-Output "MDE discovery process completed!"
 		Write-Output "There were $($successfulConnectionsFound) successful connections "
 		Write-Output "There were $($failedConnectionsFound) failed connections"
-		Write-Output "Saving registry data to $($LoggingPath)"
-		Write-Output "Saving failed connection data to $($FailureLoggingPath)"
+		if ($parameters.ContainsKey('SaveResults')) {
+			Write-Output "Saving registry data to $($LoggingPath)"
+			Write-Output "Saving failed connection data to $($FailureLoggingPath)"
+		}
+		Write-Output "MDE discovery process completed!"
 	}
 }
--- README.md
+++ README.md
@@ -6,10 +6,24 @@ Find MDE device onboarding status
 
 Copy this script down and save it to a local directory and run the following command: Import-Module Find-MDEDeviceState.ps1
 
+## Examples
+
 - EXAMPLE 1: Find-MDEDeviceState -DisableProgressBar
 
      This will disable the progress bar in the UI
 
-- EXAMPLE 2: Find-MDEDeviceState -Verbose
+- EXAMPLE 2: Find-MDEDeviceState -ComputerName MachineOne, MachineTwo
 
-    This will run the script in verbose mode
+     This will query for both MachineOne and MachineTwo
+
+- EXAMPLE 3: Find-MDEDeviceState -Verbose
+
+     This will run the script in verbose mode
+
+- EXAMPLE 4: Find-MDEDeviceState -ShowResults
+
+     Display search results to the console
+
+- EXAMPLE 5: Find-MDEDeviceState -SaveResults
+
+     Query for MDE information and save results to disk
